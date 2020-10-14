82e91a44135eb8bab428593ff29fb49d4a4bfbdb
whataretheboysanime <alexletunov96@gmail.com>
2020-10-14 21:40:09 +0300

    fix logic equal links

--- content.js
+++ content.js
@@ -1,10 +1,9 @@
 chrome.runtime.sendMessage("Empty");
 chrome.runtime.onMessage.addListener(function(message) {
-    if(message[0]['is execute'] == 'true' && message[1]['href'] == window.location.href) {
+    if(message[0]['is execute'] == 'true' && window.location.href.indexOf(message[1]['href']) > -1) {
         var totp = new jsOTP.totp();
     
         document.querySelector("#\\32 fpin").value = totp.getOtp(message[2]['secure code']);
-        
         document.querySelector("input.aui-button.aui-button-primary[type=submit]").click();
     }
 });
